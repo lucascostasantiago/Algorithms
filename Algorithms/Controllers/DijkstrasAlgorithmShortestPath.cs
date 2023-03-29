97827bbd98c94aa9572678d64818fbe7c6df51f3
Lucas Santiago <lcscostasantiago@gmail.com>
2023-03-28 21:41:43 -0300

    cria v1 algoritmo de dijkstra.

---
+++ Algorithms/Controllers/DijkstrasAlgorithmShortestPath.cs
@@ -0,0 +1,23 @@
+﻿using Algorithms.Algorithms.DijkstrasAlgorithmShortestPath;
+using Algorithms.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Algorithms.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DijkstrasAlgorithmShortestPathController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult RunAlgorithm([FromBody] GraphViewModel vm)
+        {
+            if (vm.Method == 1)
+            {
+                var result = AlgorithmV1.Run(vm); // mostra a distancia do vertice inicial ao vertice final;
+                return Ok(result);
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ Algorithms/ViewModel/GraphViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms.ViewModel
+{
+    public class GraphViewModel
+    {
+        public string Graph { get; set; }
+        public string StartVertex { get; set; }
+        public string EndVertex { get; set; }
+        public int Method { get; set; }
+    }
+}
